bf282967fa2ddd6e3ea351bd6921663264a0b27e
Mark Lannen <marklannendev@gmail.com>
2024-01-31 20:54:58 -0700

    add example conditional logic

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
         <?php $name = "Dark Matter";
         $read = true;
         if ($read) {
-            $message = "You have read $name";
+            $message = "You have read '$name'";
         } else {
             $message = "You have not read $name";
         }
