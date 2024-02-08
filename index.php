3f7cf9d27ba218c1558566705b8eba2a2ce69abb
Mark Lannen <marklannendev@gmail.com>
2024-02-07 21:37:28 -0700

    separate php logic into separate file index.php and create new file index.view.php for html and view

--- index.php
+++ index.php
@@ -1,70 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-    <head>
-        <meta charset="UTF-8" />
-        <title>Demo</title>
-        <style>
-            body {
-                display: grid;
-                place-items: center;
-                height: 100vh;
-                margin: 0;
-                font-family: sans-serif;
-            }
-        </style>
-    </head>
+$books = [
+    [
+        'name' => 'Do Androids Dream of Electric Sheep',
+        'author' => 'Phillip K. Dick',
+        'releaseYear' => 1968,
+        'purchaseUrl' => 'www.example.com'
+    ],
+    [
+        'name' => 'Project Hail Mary',
+        'author' => 'Andrew Weir',
+        'releaseYear' => 2021,
+        'purchaseUrl' => 'www.example.com'
+    ],
+    [
+        'name' => 'The Martian',
+        'author' => 'Andrew Weir',
+        'releaseYear' => 2011,
+        'purchaseUrl' => 'www.example.com'
+    ]
+];
+function filter($items, $fn)
+{
+    $filteredItems = [];
 
-    <body>
-        <h1>
-            Recommended Books
-        </h1>
-        <?php $books = [
-            [
-                'name' => 'Do Androids Dream of Electric Sheep',
-                'author' => 'Phillip K. Dick',
-                'releaseYear' => 1968,
-                'purchaseUrl' => 'www.example.com'
-            ],
-            [
-                'name' => 'Project Hail Mary',
-                'author' => 'Andrew Weir',
-                'releaseYear' => 2021,
-                'purchaseUrl' => 'www.example.com'
-            ],
-            [
-                'name' => 'The Martian',
-                'author' => 'Andrew Weir',
-                'releaseYear' => 2011,
-                'purchaseUrl' => 'www.example.com'
-            ]
-        ];
-        function filter($items, $fn)
-        {
-            $filteredItems = [];
-
-            foreach ($items as $item)
-                if ($fn($item)) {
-                    $filteredItems[] = $item;
-                }
-            return $filteredItems;
+    foreach ($items as $item)
+        if ($fn($item)) {
+            $filteredItems[] = $item;
         }
+    return $filteredItems;
+}
 
 
-        $filteredBooks = array_filter($books, function ($book) {
-            return $book['author'] === 'Andrew Weir';
-        });
+$filteredBooks = array_filter($books, function ($book) {
+    return $book['author'] === 'Andrew Weir';
+});
 
-        ?>
-        <ul>
-            <?php foreach ($filteredBooks as $book): ?>
-                <li><a href="<?= $book['purchaseUrl'] ?>">
-                        <?= $book['name']; ?>
-                    </a>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-
-    </body>
-
-</html>
+require "index.view.php";
